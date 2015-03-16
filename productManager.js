d9faed0bf410711f9de8f4b6742fa38714e7a017
Mawande06 <mawande@projectcodex.co>
2015-03-16 16:17:18 +0200

    Split into categories and got totals

--- productManager.js
+++ productManager.js
@@ -36,40 +36,42 @@ exports.returnsLeastSellingProd = function(productsSoldMap){
   	return leastSold;	
 }
 
-exports.returnsLeastSellingCat = function(productsSoldMap){
+exports.returnsMostSellingCat = function(categoryMap){
 
-	var leastSold = { name : "" ,
-				qty : Number.POSITIVE_INFINITY};
+	var mostSellingCat = { name : "" ,
+					 qty : 0};
 
 	// ??
-	for(var product in productsSoldMap){
-    	if(productsSoldMap.hasOwnProperty(product)){
-      		var qtySold = productsSoldMap[product];
-	       	if( qtySold < leastSold.qty){
-             	leastSold.qty = qtySold;
-             	leastSold.name = product;
-	        }         
-		}
+	for(category in categoryMap){
+		 var qtySold = categoryMap[category.qty];
+     	if(categoryMap.hasOwnProperty(category)){
+       		qtySold = categoryMap[category.qty];
+	        	if(qtySold > mostSellingCat.qty){
+	       		
+              	mostSellingCat.qty = qtySold;
+              	mostSellingCat.name = product;
+             	
+	         }    
+		 }
 	}
-
-  	return leastSold;	
+  return mostSellingCat; 
+  		
 }
-
 exports.returnsLeastSellingCat = function(productsSoldMap){
 
-	var leastSold = { name : "" ,
+	var leastSellingCat = { name : "" ,
 				qty : Number.POSITIVE_INFINITY};
 
 	// ??
 	for(var product in productsSoldMap){
     	if(productsSoldMap.hasOwnProperty(product)){
       		var qtySold = productsSoldMap[product];
-	       	if( qtySold < leastSold.qty){
-             	leastSold.qty = qtySold;
-             	leastSold.name = product;
+	       	if( qtySold < leastSellingCat.qty){
+             	leastSellingCat.qty = qtySold;
+             	leastSellingCat.name = product;
 	        }         
 		}
 	}
 
-  	return leastSold;	
+  	return leastSellingCat;	
 }
--- table.js
+++ table.js
@@ -8,39 +8,64 @@ var csvFileName="./Sales.csv";
 var fileStream=fs.createReadStream(csvFileName);
 //new converter instance
 var csvConverter =new Converter({constructResult:true});
-
-var fruit ={};
-var	bathroom ={};
-var	drinks={};
-var	giftItems={};
-var	stapleFoods={};
-var categories = [fruit,bathroom,drinks,giftItems,stapleFoods];
-var test = [];
+var categoryMapping = {
+  
+    'Milk 1l': 'stapleFoods',
+    'Imasi' : 'stapleFoods',
+    'Bread' : 'stapleFoods',
+    'Chakalaka Can': 'nonPerishables',
+    'Gold Dish Vegetable Curry Can': 'nonPerishables',
+    'Fanta 500ml': 'drinks',
+    'Coke 500ml': 'drinks',
+    'Cream Soda 500ml': 'drinks',
+    'Iwisa Pap 5kg': 'stapleFoods',
+    'Top Class Soy Mince': 'nonPerishables',
+    'Shampoo 1 litre': 'bathroom',
+    'Soap Bar': 'bathroom',
+    'Bananas - loose': 'fruit',
+    'Apples - loose': 'fruit',
+    'Mixed Sweets 5s': 'giftItems',
+    'Heart Chocolates': 'giftItems',
+    'Rose (plastic)': 'giftItems',
+    'Valentine Cards': 'giftItems'
+};
 
 //end_parsed will be emitted once parsing finished
 csvConverter.on("end_parsed",function(products){
    //console.log(products); //here is your result json object
   	var productsSoldMap = {};
-	products.forEach(function(product){
-    	var productName = product["stock item"];
+    
+	  products.forEach(function(product){
+    var productName = product["stock item"];
 		if(!productsSoldMap[productName]){
 			productsSoldMap[productName] = 0;
       	}
     	productsSoldMap[productName] = productsSoldMap[productName] + product["No sold"];
-    	var prod = productsSoldMap[productName];
-    	if (prod === "Creme Soda 500ml" || prod === "Coke 500ml" || prod === "Fanta 500ml" ) {
-    		test = test.push(prod);
-    	};
-            console.log(prod);
+
 	});
+  
+  var categoryMap={};
+  products.forEach(function(product){
 
-	console.log(test);
+    
+      var qty = product["No sold"];
+      var categoryName = categoryMapping[product["stock item"]];
+      if(!categoryMap[categoryName]){
+        categoryMap[categoryName] = 0;
+      }
+     
+      categoryMap[categoryName] = categoryMap[categoryName] +qty;
+    
+  });
+
+	console.log(categoryMap);
 	console.log(productsSoldMap);
-
+  console.log(productManager.returnsMostSellingCat(categoryMap));
 		//console.log(productsSoldMap);
     	//console.log(productManager.returnsMostSellingProd(productsSoldMap));
  		//console.log(productManager.returnsLeastSellingProd(productsSoldMap));
 });
+
 
 //read from file
 fileStream.pipe(csvConverter);
