762b4f1602c075514fd1654fb71066d029fced20
Mawande06 <mawande@projectcodex.co>
2015-03-18 10:42:38 +0200

    Fixed tests for Travis-CI!

--- test/spazaTest.js
+++ test/spazaTest.js
@@ -6,13 +6,13 @@ QUnit.test("testing mostSold function", function(assert){
     // var value = soldMost(objToTest);
     // is the result as we expected?
     var result1 = pm.returnsMostSellingProd(objToTest);
-    assert.deepEqual(result1, { name : 'grapes', qty : 8});
+    assert.deepEqual(result1, {name : 'grapes', qty : 8});
     
 });
 
 QUnit.test("testing leastSold function", function(assert){
 
-    var objToTest = {'apples':1,'bananas':3,'grapes':8};
+    var objToTest = {'apples':1,'bananas': 3,'grapes': 8};
    // var value = soldMost(objToTest);
     // is the result as we expected?
     var result2 = pm.returnsLeastSellingProd(objToTest);
@@ -26,12 +26,13 @@ QUnit.test("testing most selling category function", function(assert){
 
     var objToTest = {"freshFruit":12,"freshFlowers":122,"team":202};
     var result3 = pm.returnsMostSellingCat(objToTest);
-    assert.deepEqual(result3, {name:'team' ,qty: 202});
+    assert.deepEqual(result3, {"name":"team" ,"qty": 202});
 });
 
 QUnit.test("testing least selling category function", function(assert){
 
 var objToTest = {"freshFruit":12,"freshFlowers":122,"team":202};
     var result4 = pm.returnsLeastSellingCat(objToTest);
-    assert.deepEqual(result4, {name:'freshFruit',qty: 12});
+    assert.deepEqual(result4, {"name":"freshFruit","qty": 12});
+    
 });
